a85f0aead14b150c6eee92038c9fa5f20dd7538a
Denilson Ferreira de Araujo <denilson.araujo.programador@gmail.com>
2025-02-06 20:56:54 -0300

    Ajuste para deploy da metadadata2

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,137 +1,132 @@
-import { GameProps } from "@/utils/types/game"
-import { redirect } from "next/navigation"
-import Image from "next/image"
-import { Container } from "@/components/container"
-import { Label } from "./components/label"
-import { GameCard } from "@/components/GameCard"
-import { Metadata, ResolvingMetadata } from "next";
-import { PageProps } from "next";
+import { GameProps } from "@/utils/types/game";
+import { redirect } from "next/navigation";
+import Image from 'next/image'
+import { Container } from '@/components/container'
+import { Label } from './components/label'
+import { GameCard } from "@/components/GameCard";
+import { Metadata } from 'next'
 
-// interface PropsParams {
-//     params: {
-//         id: string,
-//     }
-// }
+interface PropsParams {
+  params: {
+    id: string;
+  }
+}
 
-// export async function generateMetadata({ params }: PropsParams): Promise<Metadata> {
-
-export async function generateMetadata(
-    { params }: { params: { id: string } },
-    parent: ResolvingMetadata
-): Promise<Metadata> {
-    try {
-        const response: GameProps = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${params.id}`, { next: { revalidate: 60 } })
-            .then((res) => res.json())
-            .catch(() => {
-                return {
-                    title: "DalyGames - Descubra jogos incríveis para se divertir."
-                }
-            })
-
-
-
+export async function generateMetadata({ params }: PropsParams): Promise<Metadata> {
+  try {
+    const response: GameProps = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${params.id}`, { next: { revalidate: 60 } })
+      .then((res) => res.json())
+      .catch(() => {
         return {
-            title: response.title,
-            description: `${response.description.slice(0, 100)}...`,
-            openGraph: {
-                title: response.title,
-                images: [response.image_url]
-            }
+          title: "DalyGames - Descubra jogos incríveis para se divertir."
         }
+      })
 
-
-
-    } catch (err) {
-        return {
-            title: "DalyGames - Descubra jogos incríveis para se divertir."
+    return {
+      title: response.title,
+      description: `${response.description.slice(0, 100)}...`,
+      openGraph: {
+        title: response.title,
+        images: [response.image_url]
+      },
+      robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+          index: true,
+          follow: true,
+          noimageindex: true,
         }
+      }
     }
 
 
-}
 
+  } catch (err) {
+    return {
+      title: "DalyGames - Descubra jogos incríveis para se divertir."
+    }
+  }
+}
 
 
 async function getData(id: string) {
-    try {
-        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, { next: { revalidate: 60 } })
-
-        return res.json()
-
-    } catch (err) {
-        throw new Error("Failed to fetch data.")
-    }
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game&id=${id}`, { next: { revalidate: 60 } })
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data")
+  }
 }
 
 async function getGameSorted() {
-    try {
-        const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { cache: "no-store" })
-
-        return res.json();
-
-    } catch (err) {
-        throw new Error("failed do fatch data getGameSorted.")
-    }
+  //https://sujeitoprogramador.com/next-api/?api=game_day
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, { cache: "no-store" })
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data")
+  }
 }
 
-// export default async function Game({ params: { id } }: { params: { id: string } }) {
 
-export default async function Game({ params }: { params: { id: string } }) {
-    const id = params.id as string;
+export default async function Game({
+  params: { id }
+}: {
+  params: { id: string }
+}) {
+  const data: GameProps = await getData(id)
+  const sortedGame: GameProps = await getGameSorted();
 
-    const data: GameProps = await getData(id)
-    const sortedGame: GameProps = await getGameSorted()
 
-    console.log(sortedGame)
 
-    if (!data) {
-        redirect("/")
-    }
+  if (!data) {
+    redirect("/")
+  }
 
-    return (
-        <main className="w-full text-black">
+  return (
+    <main className="w-full text-black">
+      <div className="bg-black h-80 sm:h-96 w-full relative">
+        <Image
+          className="object-cover w-full h-80 sm:h-96 opacity-75"
+          src={data.image_url}
+          alt={data.title}
+          priority={true}
+          fill={true}
+          quality={100}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 44vw"
+        />
+      </div>
 
-            <div className="bg-black h-80 sm:h-96 w-full relative">
-                <Image
-                    className="object-cover w-full h-80 sm:h-96 opacity-80"
-                    src={data.image_url}
-                    alt={data.title}
-                    priority={true}
-                    fill={true}
-                    sizes='(max-width: 798px) 100vw, (max-width: 1200px) 44vw'
-                />
-            </div>
+      <Container>
+        <h1 className="font-bold text-xl my-4">{data.title}</h1>
+        <p>{data.description}</p>
 
-            <Container>
-                <h1 className="font-bold text-xl my-4">{data.title}</h1>
-                <p>{data.description}</p>
+        <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
+        <div className="flex gap-2 flex-wrap">
+          {data.platforms.map((item) => (
+            <Label name={item} key={item} />
+          ))}
+        </div>
 
-                <h2 className="font-bold text-lg mt-7 mb-2">Plataformas</h2>
-                <div className="flex gap-2 flex-wrap">
-                    {data.platforms.map((item) => (
-                        <Label name={item} key={item} />
-                    ))}
-                </div>
+        <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
+        <div className="flex gap-2 flex-wrap">
+          {data.categories.map((item) => (
+            <Label name={item} key={item} />
+          ))}
+        </div>
 
-                <h2 className="font-bold text-lg mt-7 mb-2">Categorias</h2>
-                <div className="flex gap-2 flex-wrap">
-                    {data.categories.map((item) => (
-                        <Label name={item} key={item} />
-                    ))}
-                </div>
+        <p className="mt-7 mb-2"><strong>Data de lançamento:</strong> {data.release}</p>
 
-                <p className="mt-7 mb-2"><strong> Data de Lançamento:</strong> {data.release} </p>
+        <h2 className="font-bold text-lg mt-7 mb-2">Jogo recomendado:</h2>
+        <div className="flex">
+          <div className="flex-grow">
+            <GameCard data={sortedGame} />
+          </div>
+        </div>
 
-                <h2 className="font-bold text-lg mt-7 mb-2">Jogo recomendado</h2>
-
-                <div className="flex">
-                    <div className="flex-grow hover:scale-100">
-                        <GameCard data={sortedGame} />
-                    </div>
-                </div>
-
-            </Container>
-
-        </main>
-    )
+      </Container>
+    </main>
+  )
 }
